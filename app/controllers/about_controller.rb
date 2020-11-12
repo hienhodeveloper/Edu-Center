f6604bf312670802dd57516f5334caf852ef55a6
Hien Ho <hien.ho@c2c-techhub.io>
2020-11-12 09:59:33 +0700

    add base view

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    flash[:danger] = "hello adsbsamd basmbdamb dsadbasb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'about/index'
   get 'sign_up/index'
   get 'login/index'
   get 'home/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+
+  resources :home do 
+    
+  end
+
+  root 'home#index'
 end
