a5b15e4ae2d39d4e7194433eb530d2e7755b0a96
Hien Ho <hien.ho@c2c-techhub.io>
2020-11-15 20:43:55 +0700

    added login and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,6 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-
-  validates :username,
-    presence: true, 
-    uniqueness: { case_sensitive: false }, 
-    length: { minimum: 3, maximum: 25}
-  
-  validates :email, 
-    presence: true,
-    uniqueness: true, 
-    format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email" } 
-
-  validates :user_type, 
-    presence: true,
-    inclusion: { in: ["STUDENT", "TEACHER", "ADMIN"] }
-
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
   get 'about/index'
   get 'login/index'
   get 'home/index'
