42df73e6066b4f10507e8619f3e338521f478281
Hien Ho <hien.ho@c2c-techhub.io>
2020-11-18 23:50:23 +0700

    added google login

--- Gemfile
+++ Gemfile
@@ -21,13 +21,15 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
+gem "figaro"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
   
   validates :full_name,
     presence: true, 
     length: { maximum: 25 }
+
+  def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+      if user
+        user
+      else
+        user = User.create(full_name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20],
+           uid: access_token[:uid],
+           provider: access_token[:provider]
+        )
+      end
+  end
 end
