4bd3aa13b01e1f0e090849651f6ac97f93e89373
Hien Ho <hien.ho@c2c-techhub.io>
2020-12-15 22:58:46 +0700

    fix login 2fa

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 class Users::SessionsController < Devise::SessionsController
   include AuthenticateWithOtpTwoFactor
-  layout 'login'
 
   prepend_before_action :authenticate_with_otp_two_factor,
                         if: -> { action_name == 'create' && otp_two_factor_enabled? }
